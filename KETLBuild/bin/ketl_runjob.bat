11e349a5751b1770ea8f5532bce46cebd6ed0c2e
nwakefield <nwakefield@d2c3c37a-7724-0410-8b59-f4172f08b12f>
2008-04-02 16:22:24 +0000

    fix

--- KETLBuild/bin/ketl_runjob.bat
+++ KETLBuild/bin/ketl_runjob.bat
@@ -5,7 +5,7 @@ if not defined KETLDIR goto ERROR
 
 cd "%KETLDIR%"
 
-echo "%JAVA_HOME%\bin\java" %JAVA_OPTS% "-Dlog4j.configuration=file:%KETLDIR%\conf\KETL.log.properties"  -classpath "%KETLDIR%\lib\KETL.jar"  RunJob FILE=%CURRENT_DIR%\%1%
+echo "%JAVA_HOME%\bin\java" %JAVA_OPTS% "-Dlog4j.configuration=file:%KETLDIR%\conf\KETL.log.properties"  -classpath "%JAVA_HOME\lib\tools.jar;%KETLDIR%\lib\KETL.jar"  RunJob FILE=%CURRENT_DIR%\%1%
 cd "%CURRENT_DIR%"
 GOTO END
 
